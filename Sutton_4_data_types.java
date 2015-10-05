997ace4d9c67dd6e1ee32e64378d48f027d5f346
Coby Sutton <dustor7689@gmail.com>
2015-10-05 07:21:17 -0500

    My data types in java code.

---
+++ Sutton_4_data_types.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sutton_4_september;
+
+/**
+ *
+ * @author dustor7689
+ */
+public class Sutton_4_data_types {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Space for comments
+        //Used to explain code 
+        //Code explanation is an expectation
+        //in d22
+    int i = 0;
+    float a = 7.11f;
+    double b = 11.0;
+    short c = 100;
+    char d1 = '1';
+    char d2 = 'd';
+    char d3 = 122;
+    String e = "Why certainly";
+    String el = "Coby is the best";
+    boolean f = false;
+    boolean g = i % 2 == 0;
+/*
+    here we will look at some mathematical 
+    operators
+    */
+// + this adds two numbers and more 
+// - this subtracts two numbers and more 
+// * this multiplies two numbers and more    
+// / this is divides two numbers and more    
+// % this is a modulo. Shows you the remainder
+    
+   /* System.out.println(i + " an integer");
+    System.out.println(a + " a float");
+    System.out.println(b + " a double");
+    System.out.println(c + " a short");
+    System.out.println(d1 + " a char");
+    System.out.println(d2 + " a char");
+    System.out.println(d3 + " a char");
+    System.out.println(e + " a string");
+    System.out.println(f + " a boolean");*/
+    for(i = 0;i < 500; i++){
+        //if is a conditional (flow) statement. If the condition is true then the following command will be executed.
+        if(i%2==0){
+        System.out.println(i + " is even");
+              } else {
+           System.out.println(i + " is odd"); 
+        }
+         }
+//            System.out.println(i % 3 == 0);
+                //% checks for the remainder
+    // i % 2 checks if number is even or odd 
+   // i = 0;
+    //a = 0;
+  //  e = "ninja";
+   // System.out.println(i + a + " an integer" + "plus a float ");
+   // System.out.println(e + i + " an /" + "a string plus a integer ");
+    //upcast to float is automatic
+    //i.e. 7 + 0.0 = 7.0
+//    i =22;
+//    a = -12;
+//    e="i am";
+    
+    }
+
+    }
+    
+//}
